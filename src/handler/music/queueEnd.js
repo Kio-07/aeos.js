a8548a89af624c6b3582b8c1aef6d483be13b909
Kio-07 <123291093+Kio-07@users.noreply.github.com>
2023-01-27 05:00:22 +0530

    Add files via upload

---
+++ src/handler/music/queueEnd.js
@@ -0,0 +1,49 @@
+const { aeosError } = require("../../index.js");
+const Interpreter = require("../../interpreter.js");
+
+module.exports = async (Channel, client, voice) => {
+  const cmds = voice.cmd.queueEnd.allValues();
+  for (const cmd of cmds) {
+    const id = cmd.channel.includes("$")
+      ? (
+          await Interpreter(
+            client,
+            {
+              channel: Channel,
+              guild: Channel.guild,
+            },
+            [],
+            {
+              name: "channelParser",
+              code: cmd.channel,
+            },
+            client.db,
+            true,
+          )
+        )?.code
+      : cmd.channel;
+    const channel = client.channels.cache.get(id);
+    if (!channel)
+      return aeosError.CommandError(
+        cmd,
+        "channel",
+        cmd.name,
+        cmds.findIndex((c) => !c.channel),
+      );
+
+    await Interpreter(
+      client,
+      {
+        channel: Channel,
+        guild: Channel.guild,
+      },
+      [],
+      cmd,
+      client.db,
+      false,
+      undefined,
+      {},
+      channel,
+    );
+  }
+};
